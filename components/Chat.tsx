bea7b92243ae3f5f74471212da11ac95c245a17a
hayri32 <189244213+hayri32@users.noreply.github.com>
2024-11-22 23:10:31 +0300

    Update Chat.tsx

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -16,16 +16,25 @@ export default function ClientComponent({
   
   return (
     <div className="relative grow flex flex-col mx-auto w-full overflow-hidden h-[0px]">
-      <VoiceProvider
-        auth={{ 
-          type: "accessToken", 
-          value: accessToken 
-        }}
-      >
-        <Messages ref={ref} />
-        <Controls />
-        <StartCall />
-      </VoiceProvider>
+      {/* Header */}
+      <div className="fixed top-0 left-0 right-0 bg-blue-500 text-white p-4 text-center z-50">
+        <h1 className="text-xl font-bold">English Learning Assistant</h1>
+        <p className="text-sm opacity-80">Practice Speaking & Pronunciation</p>
+      </div>
+
+      {/* Main Content */}
+      <div className="mt-16"> {/* Add margin-top to account for fixed header */}
+        <VoiceProvider
+          auth={{ 
+            type: "accessToken", 
+            value: accessToken 
+          }}
+        >
+          <Messages ref={ref} />
+          <Controls />
+          <StartCall />
+        </VoiceProvider>
+      </div>
     </div>
   );
 }

dd9189f40f04bea3f237ad30b56425ba881ed28e
hayri32 <189244213+hayri32@users.noreply.github.com>
2024-11-22 21:54:13 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { GeistSans } from "geist-font/sans";
-import { GeistMono } from "geist-font/mono";
 import "./globals.css";
-import { cn } from "@/utils";
 
 export const metadata: Metadata = {
   title: "English Learning Assistant",
@@ -16,13 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          GeistSans.variable,
-          GeistMono.variable,
-          "flex flex-col min-h-screen"
-        )}
-      >
+      <body className="flex flex-col min-h-screen">
         {children}
       </body>
     </html>
